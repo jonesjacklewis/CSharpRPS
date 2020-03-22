28240089c2415ec78fdfe683649a5f94295cf8d1
Jack Jones <44882194+jonesjacklewis@users.noreply.github.com>
2020-03-22 23:03:23 +0000

    Add files via upload

---
+++ NewRPS/NewRPS/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace NewRPS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.P1 = new System.Windows.Forms.Label();
+            this.P2 = new System.Windows.Forms.Label();
+            this.Message = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(215, 162);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Done";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::NewRPS.Properties.Resources.Scissors;
+            this.pictureBox4.Location = new System.Drawing.Point(344, 339);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 15;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::NewRPS.Properties.Resources.Paper;
+            this.pictureBox5.Location = new System.Drawing.Point(344, 254);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 14;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::NewRPS.Properties.Resources.Rock;
+            this.pictureBox6.Location = new System.Drawing.Point(344, 147);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox6.TabIndex = 13;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::NewRPS.Properties.Resources.Scissors;
+            this.pictureBox3.Location = new System.Drawing.Point(82, 339);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 12;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click_1);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::NewRPS.Properties.Resources.Paper;
+            this.pictureBox2.Location = new System.Drawing.Point(82, 254);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::NewRPS.Properties.Resources.Rock;
+            this.pictureBox1.Location = new System.Drawing.Point(82, 147);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(215, 254);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 22;
+            this.button2.Text = "One Player?";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_2);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(125, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Player 1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(341, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Player 2";
+            // 
+            // P1
+            // 
+            this.P1.AutoSize = true;
+            this.P1.Location = new System.Drawing.Point(125, 83);
+            this.P1.Name = "P1";
+            this.P1.Size = new System.Drawing.Size(20, 13);
+            this.P1.TabIndex = 21;
+            this.P1.Text = "P1";
+            this.P1.Click += new System.EventHandler(this.P1_Click);
+            // 
+            // P2
+            // 
+            this.P2.AutoSize = true;
+            this.P2.Location = new System.Drawing.Point(350, 83);
+            this.P2.Name = "P2";
+            this.P2.Size = new System.Drawing.Size(20, 13);
+            this.P2.TabIndex = 20;
+            this.P2.Text = "P2";
+            // 
+            // Message
+            // 
+            this.Message.AutoSize = true;
+            this.Message.Location = new System.Drawing.Point(235, 115);
+            this.Message.Name = "Message";
+            this.Message.Size = new System.Drawing.Size(50, 13);
+            this.Message.TabIndex = 23;
+            this.Message.Text = "Message";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 440);
+            this.Controls.Add(this.Message);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.P2);
+            this.Controls.Add(this.P1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Rock Paper Scissors";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label P1;
+        private System.Windows.Forms.Label P2;
+        private System.Windows.Forms.Label Message;
+    }
+}
+
---
+++ NewRPS/NewRPS/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NewRPS
+{
+    public partial class Form1 : Form
+    {
+        string choice1;
+        string choice2;
+        bool oneplayer = false;
+        int chosen1 = 0;
+        string winner;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+       private void togglecolor(PictureBox pb)
+        {
+            if (pb.BackColor == System.Drawing.Color.Gray)
+            {
+                pb.BackColor = Form1.DefaultBackColor;
+            }
+            else
+            {
+                pb.BackColor = System.Drawing.Color.Gray;
+            }
+        }
+
+       
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            choice1 = "Scissors";
+            chosen1 += 1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Location = new Point((Form1.ActiveForm.Width / 2) - 50, 10);
+            pictureBox1.Location = new Point((Form1.ActiveForm.Width / 2) - 50, pictureBox1.Location.Y);
+            pictureBox2.Location = new Point((Form1.ActiveForm.Width / 2) - 50, pictureBox2.Location.Y);
+            pictureBox3.Location = new Point((Form1.ActiveForm.Width / 2) - 50, pictureBox3.Location.Y);
+            button2.Hide();
+            pictureBox4.Hide();
+            pictureBox5.Hide();
+            pictureBox6.Hide();
+            oneplayer = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (oneplayer == true || choice2 == null)
+            {
+                var list = new List<string> { "Rock", "Paper", "Scissors" };
+                Random rnd = new Random();
+                int r = rnd.Next(list.Count);
+                choice2 = list[r];
+               
+            }
+            if (choice1 == choice2)
+            {
+                winner = "It's a draw";
+            }
+            else if (choice1 == "Rock" && choice2 == "Paper")
+            {
+                winner = "Player 2 Wins";
+            }
+            else if (choice1 == "Rock" && choice2 == "Scissors")
+            {
+                winner = "Player 1 Wins";
+            }
+            else if (choice1 == "Paper" && choice2 == "Rock")
+            {
+                winner = "Player 1 Wins";
+            }
+            else if (choice1 == "Paper" && choice2 == "Scissors")
+            {
+                winner = "Player 2 Wins";
+            }
+            else if (choice1 == "Scissors" && choice2 == "Rock")
+            {
+                winner = "Player 2 Wins";
+            }
+            else if (choice1 == "Scissors" && choice2 == "Paper")
+            {
+                winner = "Player 1 Wins";
+            }
+            else
+            {
+                winner = "Sorry something has gone wrong. Please reload";
+            }
+
+            P1.Text = choice1;
+            P2.Text = choice2;
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            choice1 = "Paper";
+            chosen1 += 1;
+            togglecolor(pictureBox2);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            choice1 = "Rock";
+            chosen1 += 1;
+            togglecolor(pictureBox1);
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            button1.Location = new Point((Form1.ActiveForm.Width / 2) - 50, 10);
+            pictureBox1.Location = new Point((Form1.ActiveForm.Width / 2) - 50, pictureBox1.Location.Y);
+            pictureBox2.Location = new Point((Form1.ActiveForm.Width / 2) - 50, pictureBox2.Location.Y);
+            pictureBox3.Location = new Point((Form1.ActiveForm.Width / 2) - 50, pictureBox3.Location.Y);
+            button2.Hide();
+            pictureBox4.Hide();
+            pictureBox5.Hide();
+            pictureBox6.Hide();
+            oneplayer = true;
+        }
+
+        private void button2_Click_2(object sender, EventArgs e)
+        {
+            button1.Location = new Point((Form1.ActiveForm.Width / 2) - 50, 10);
+            pictureBox1.Location = new Point((Form1.ActiveForm.Width / 2) - 50, pictureBox1.Location.Y);
+            pictureBox2.Location = new Point((Form1.ActiveForm.Width / 2) - 50, pictureBox2.Location.Y);
+            pictureBox3.Location = new Point((Form1.ActiveForm.Width / 2) - 50, pictureBox3.Location.Y);
+            button2.Hide();
+            pictureBox4.Hide();
+            pictureBox5.Hide();
+            pictureBox6.Hide();
+            oneplayer = true;
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+            choice2 = "Rock";
+            togglecolor(pictureBox6);
+
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            togglecolor(pictureBox5);
+            choice2 = "Paper";
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            togglecolor(pictureBox4);
+            choice2 = "Scissors";
+        }
+
+        private void P1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (oneplayer == true || choice2 == null)
+            {
+                var list = new List<string> { "Rock", "Paper", "Scissors" };
+                Random rnd = new Random();
+                int r = rnd.Next(list.Count);
+                choice2 = list[r];
+
+            }
+            if (choice1 == choice2)
+            {
+                winner = "It's a draw";
+            }
+            else if (choice1 == "Rock" && choice2 == "Paper")
+            {
+                winner = "Player 2 Wins";
+            }
+            else if (choice1 == "Rock" && choice2 == "Scissors")
+            {
+                winner = "Player 1 Wins";
+            }
+            else if (choice1 == "Paper" && choice2 == "Rock")
+            {
+                winner = "Player 1 Wins";
+            }
+            else if (choice1 == "Paper" && choice2 == "Scissors")
+            {
+                winner = "Player 2 Wins";
+            }
+            else if (choice1 == "Scissors" && choice2 == "Rock")
+            {
+                winner = "Player 2 Wins";
+            }
+            else if (choice1 == "Scissors" && choice2 == "Paper")
+            {
+                winner = "Player 1 Wins";
+            }
+            else
+            {
+                winner = "Sorry something has gone wrong. Please reload";
+            }
+
+            P1.Text = choice1;
+            P2.Text = choice2;
+            Message.Text = winner;
+
+        }
+
+        private void pictureBox3_Click_1(object sender, EventArgs e)
+        {
+            choice1 = "Scissors";
+            chosen1 += 1;
+            togglecolor(pictureBox3);
+        }
+
+        
+    }
+}
